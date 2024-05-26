80c050dc5ff255f75bb8e43443f344f8afadcd81
drewhouses <drewcasas@live.com>
2024-05-26 14:34:20 -0500

    Basic structure and components created

--- weatherapp/src/app/app.component.ts
+++ weatherapp/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CurrentWeather } from './components/current-weather.component';
+import { PageHeader } from './components/header.component';
+import { ForecastWeather } from './components/forecast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, CurrentWeather, PageHeader, ForecastWeather],
+  template: `
+    <page-header></page-header>
+    <current-weather></current-weather>
+    <forecast-weather></forecast-weather>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ weatherapp/src/app/components/current-weather.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'current-weather',
+  standalone: true,
+  template: `
+  <p>This my current weather component<p>
+  `,
+  styleUrl: '../app.component.css'
+})
+export class CurrentWeather {
+  //props
+};
