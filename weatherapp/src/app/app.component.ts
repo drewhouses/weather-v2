e8bf4e0d70c9ddbff97cf59253afa61f6fa58509
drewhouses <drewcasas@live.com>
2024-06-01 12:05:18 -0500

    moved components to app.component.html

--- weatherapp/src/app/app.component.ts
+++ weatherapp/src/app/app.component.ts
@@ -8,14 +8,15 @@ import { SearchBar } from './components/search-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CurrentWeather, PageHeader, ForecastWeather, SearchBar],
-  template: `
-    <page-header></page-header>
-    <search-bar></search-bar>  
-    <current-weather></current-weather>
-    <forecast-weather></forecast-weather>
-  `,
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    CurrentWeather,
+    PageHeader,
+    ForecastWeather,
+    SearchBar,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Weather App';
